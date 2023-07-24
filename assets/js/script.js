b924fc445ae5c69151feb9d1c36416426dbec1e8
Sebastián Fernández <sebacontacto@gmail.com>
2023-07-24 01:32:27 -0400

    version 1.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 // inicializacion de popovers
-
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+//
 
+// modal para cada load de url
 const myModal = bootstrap.Modal.getOrCreateInstance('#welcomeModal');
 
 window.addEventListener('DOMContentLoaded', () => {
   myModal.show();
 })
+//
